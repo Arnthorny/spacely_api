ae8e3f88067f4c7ce14a52334f581de05a6d34f0
Arnthorny <chidubem.anthony26@gmail.com>
2025-04-19 07:44:35 +0100

    chore: Removed orgId parameter from OAS documentation

--- src/routes/v1/organisation.route.js
+++ src/routes/v1/organisation.route.js
@@ -101,13 +101,6 @@ module.exports = router;
  *       - bearerAuth: []
  *     parameters:
  *       - in: path
- *         name: orgId
- *         required: true
- *         description: Organisation Id
- *         schema:
- *           type: string
- *           example: 60d21b4667d0d8992e610c85
- *       - in: path
  *         name: inviteId
  *         required: true
  *         description: Invite Request Id
@@ -138,13 +131,6 @@ module.exports = router;
  *     security:
  *       - bearerAuth: []
  *     parameters:
- *       - in: path
- *         name: orgId
- *         required: true
- *         description: Organisation Id
- *         schema:
- *           type: string
- *           example: 60d21b4667d0d8992e610c85
  *       - in: path
  *         name: inviteId
  *         required: true
