8adddfe61d76f27b29cd2991ab53ba260c583406
Arnthorny <chidubem.anthony26@gmail.com>
2025-04-21 13:42:09 +0100

    chore: Update env variables

--- src/docs/swagger-options.js
+++ src/docs/swagger-options.js
@@ -22,7 +22,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.SERVER_URI || 'http://localhost:3000',
+        url: process.env.SERVER_URL || 'http://localhost:3000',
         description: 'Development server',
       },
     ],
